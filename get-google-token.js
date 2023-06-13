94d4dded023f02bb86e1838ed4701657d61e507b
James Moura <james_moura@live.com>
2023-06-13 02:22:29 -0300

    Updates GDrive

---
+++ get-google-token.js
@@ -0,0 +1,23 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+
+async function authorizeWithPrivateKey() {
+  const privateKey = fs.readFileSync('google-private-key.pem', 'utf8'); // Substitua pelo caminho para o arquivo da chave privada
+  const clientEmail = 'polyvoices@my-n8n-project-384902.iam.gserviceaccount.com'; // Substitua pelo seu e-mail de cliente
+
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      private_key: privateKey,
+      client_email: clientEmail,
+    },
+    scopes: ['https://www.googleapis.com/auth/drive'], // Substitua pelas permissões necessárias
+  });
+
+  const client = await auth.getClient();
+  const token = await client.getAccessToken();
+
+  console.log('Client de acesso:', client);
+  console.log('Token de acesso:', token);
+}
+
+authorizeWithPrivateKey().catch(console.error);
--- routes/audio.js
+++ routes/audio.js
@@ -1,4 +1,3 @@
-// functionality of a route
 const authenticate = require("../auth");
 const express = require('express');
 const ytdl = require('ytdl-core');
@@ -12,22 +11,34 @@ const app = express();
 app.use('/download', express.static('downloads'));
 const audio = require("express").Router();
 
-// Configurar as credenciais do Google Drive
-const credentials = 'polyvoices-n8n-project-384902-0f8caa90260f.json';
-const token = 'ya29.c.b0Aaekm1Iv_FT_o0egT8evkdONm4Xoc2aaYUk4B1ETdn4mdfpxhtQRkHD5dBa5BLUu7Kkgca5sKcNpxY1UQ7UkmpFj5MFNpXHiGOUlknOEb1SLK2tnRj3NVTc1ha98PGiKa9pFyT1OkH1-T3mZpKEQh01NxvqHmqv9vgPjuPB19KJLNA8QBvk8_f-W8TKLnun9vOb3JZXeJTe5DhG97VWAxHm-1Kywk8ZWJRln9B2r6tSLmrznmCYlL-BtwL0a0KH3O3qoY0gT9HRFdWbJofKXrarJUFJ80pruI8v09_5Y87KtNKf4duug4ApunotMESTtkTHEgiteE337C-Fbu3BtghsagkMtRO8xmgxSU2ZUQj8zYhFhcmbx9jb9QQJkpwcRUr1Ud2zBnioxy6Sd5c9ulFrfaQrhnY67ve2gvwqFaI675V3Xddm0fIrsJOkW0dj4ko31rqXeQgxhXZ8XhkmuY7uWsFBbZw2v2rf73c2h7R9WkbOOlrSOV9sdFlRj0mIOed32wpbcwU6ZFz8o-Us7-manjpMV8h0feek-_yerF54UF8vjMZtijxQ4xhfVI50jpvZSWZ243k_V-k9ckz9VW7Zu25cg7WnOViWvltOxh67qf9bR-kmVdsnMSgoYfku7wIIf-26VzshbY1ZO1hIsVj1kVgvmsXk8JJFhud61eouBjfqygO0lsfa5w51MO1-wrIfxarjWobZe7Ql8oFOwr22kh0d-uIfJSoy00v-UlIg0-z8v341UQs_a_BV8-xuVc9Q9Jionb0JyvOZX7XVtmiZe1li-B1lofwrs0c57Vrr7pSbpIpxg974JzS2gnJv-jg243hmSJezYtlqoeto49-QdprtwIXyeSne4Of-57lBzU-w_cxYY5dnb3ef5kOb8O_zRZ5dFStrrhpSOgtz_YsvfZxU1QciOv8ZV7rMb9_ymXivriXj7wawj0Bhmmqj-z_x7fvqtWMv2p0jJrqnMiMXUrXYqdQJ5hdXWJgpVlldU5z2oyJ3pj91';
+const privateKey = fs.readFileSync('google-private-key.pem', 'utf8'); // Substitua pelo caminho para o arquivo da chave privada
+const clientEmail = 'polyvoices@my-n8n-project-384902.iam.gserviceaccount.com'; // Substitua pelo seu e-mail de cliente
 
-// Crie um novo cliente OAuth2 com as credenciais do Google Drive
-const oAuth2Client = new google.auth.OAuth2();
-oAuth2Client.setCredentials(token);
+const jwtClient = new google.auth.JWT(clientEmail, null, privateKey, ['https://www.googleapis.com/auth/drive']);
 
-// Configurar a instância do Google Drive
-const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+const createFolder = async (folderName, parentFolderId) => {
+  const drive = google.drive({ version: 'v3', auth: jwtClient });
 
-// Função para fazer o upload do arquivo para o Google Drive
-const uploadFileToDrive = async (filePath, fileName) => {
+  const folderMetadata = {
+    name: folderName,
+    mimeType: 'application/vnd.google-apps.folder',
+    parents: [parentFolderId]
+  };
+
+  const response = await drive.files.create({
+    resource: folderMetadata,
+    fields: 'id'
+  });
+
+  return response.data.id;
+};
+
+const uploadFileToDrive = async (filePath, fileName, parentFolderId) => {
+  const drive = google.drive({ version: 'v3', auth: jwtClient });
+
   const fileMetadata = {
     name: fileName,
-    parents: ['1uYZw762ZML0WmSz6IiLtm6m793_7bYaB'] // ID da pasta de destino no Google Drive
+    parents: [parentFolderId]
   };
 
   const media = {
@@ -44,37 +55,89 @@ const uploadFileToDrive = async (filePath, fileName) => {
   return response.data.id;
 };
 
-// Requisições de áudio
+const getFolderLink = async (folderId) => {
+  const drive = google.drive({ version: 'v3', auth: jwtClient });
+
+  const response = await drive.files.get({
+    fileId: folderId,
+    fields: 'webViewLink'
+  });
+
+  return response.data.webViewLink;
+};
+
+const authorize = async () => {
+  await jwtClient.authorize();
+};
+
+const doesFileExistInDrive = async (parentFolderId, fileName) => {
+  const drive = google.drive({ version: 'v3', auth: jwtClient });
+  const response = await drive.files.list({
+    q: `'${parentFolderId}' in parents and name = '${fileName}' and trashed = false`,
+    fields: 'files(id)'
+  }).then(res => {
+    console.log(res)
+    return res.data.files.length > 0;
+  });
+};
+
+const doesFolderExistInDrive = async (parentFolderId, folderName) => {
+  const drive = google.drive({ version: 'v3', auth: jwtClient });
+
+  await drive.files.list({
+    q: `name = 'audio' and mimeType = 'application/vnd.google-apps.folder' and trashed = false`,
+    fields: 'files(id)'
+  }).then(res => {
+    console.log(res.data.files.length > 0)
+    return res.data.files.length > 0;
+  });
+};
+
 audio.get('/download/audio', authenticate, async (req, res) => {
   const youtubeUrl = req.query.url;
-  const info = await ytdl.getInfo(youtubeUrl);
-  const format = ytdl.filterFormats(info.formats, 'audioonly').find(format => format.audioBitrate === 128);
-
-  if (!format) {
+  
+  if (!ytdl.validateURL(youtubeUrl)) {
     return res.status(400).json({
-      error: 'Não foi possível encontrar um formato de áudio com 128kbps.'
+      error: 'Link incompatível, por favor verifique e tente novamente.'
     });
   }
-  const fileName = `${info.videoDetails.videoId}.mp3`;
+
+  const info = await ytdl.getInfo(youtubeUrl);
+  let format = ytdl.filterFormats(info.formats, 'audioonly').find(format => format.audioBitrate === 64);
+
+  if (!format) {
+    format = ytdl.filterFormats(info.formats, 'audioonly').find(format => format.audioBitrate === 128);
+    if (!format) {
+      return res.status(400).json({
+        error: 'Não foi possível encontrar um formato de áudio com 64kbps ou 128kbps.'
+      });
+    }
+  }
+
+  const videoTitle = info.videoDetails.title.substring(0, 15).replace(/[^\w\s]/gi, '');
+  const fileName = `${videoTitle}_${info.videoDetails.videoId}.mp3`;
   const channelName = info.videoDetails.author.name;
   const sanitizedChannelName = channelName.replace(/[^\w\s]/gi, '');
-  const filePath = path.join(__dirname, `../downloads/audio/${sanitizedChannelName}/${fileName}`);
-  const directoryPath = path.dirname(filePath);
+  const endpointFolderName = req.baseUrl.substring(1);
+  const folderName = `${endpointFolderName}_${info.videoDetails.videoId}`;
+  const basePath = path.join(__dirname, '../downloads/audio');
+  const channelPath = path.join(basePath, endpointFolderName, sanitizedChannelName);
+  const filePath = path.join(channelPath, fileName);
 
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath, { recursive: true });
+  if (!fs.existsSync(channelPath)) {
+    fs.mkdirSync(channelPath, { recursive: true });
   }
 
   if (fs.existsSync(filePath)) {
     return res.status(200).json({
       message: 'O áudio já foi baixado.',
-      downloadUrl: `${req.protocol}://${req.get('host')}/download/audio/${sanitizedChannelName}/${fileName}`
+      downloadUrl: `${req.protocol}://${req.get('host')}/download/audio/${endpointFolderName}/${sanitizedChannelName}/${fileName}`
     });
   }
 
   const writeStream = fs.createWriteStream(filePath);
   const videoStream = ytdl(youtubeUrl, { format });
-  let startTime = Date.now(); // Início do timer
+  let startTime = Date.now();
 
   videoStream.on('error', (error) => {
     console.error(error);
@@ -86,17 +149,44 @@ audio.get('/download/audio', authenticate, async (req, res) => {
   videoStream.pipe(writeStream);
 
   writeStream.on('finish', async () => {
-    let endTime = Date.now(); // Fim do timer
-    let totalTime = (endTime - startTime) / 1000; // Tempo total em segundos
+    let endTime = Date.now();
+    let totalTime = (endTime - startTime) / 1000;
 
-    // Fazer o upload do arquivo para o Google Drive
-    const fileId = await uploadFileToDrive(filePath, fileName);
+    await authorize();
+
+    const endpointFolderId = await createFolder('audio', '1uYZw762ZML0WmSz6IiLtm6m793_7bYaB');
+
+    let channelFolderId;
+    if (await doesFolderExistInDrive(endpointFolderId, 'audio')) {
+      google.drive({ version: 'v3', auth: jwtClient }).files.list({
+        q: `'name = 'audio' and mimeType = 'application/vnd.google-apps.folder' and trashed = false`,
+        fields: 'files(id)'
+      })
+    } else {
+      channelFolderId = await createFolder('audio', endpointFolderId);
+    }
+
+    if (await doesFileExistInDrive(channelFolderId, fileName)) {
+      // Exclui o arquivo local
+      fs.unlinkSync(filePath);
+
+      return res.status(200).json({
+        message: 'O áudio já foi baixado.',
+        downloadUrl: await getFolderLink(endpointFolderId)
+      });
+    }
+
+    const fileId = await uploadFileToDrive(filePath, fileName, channelFolderId);
+    const folderLink = await getFolderLink(endpointFolderId);
+
+    // Exclui o arquivo local
+    fs.unlinkSync(filePath);
 
     res.status(200).json({
       message: 'Áudio baixado e enviado para o Google Drive com sucesso!',
-      downloadUrl: `${req.protocol}://${req.get('host')}/download/audio/${sanitizedChannelName}/${fileName}`,
-      timer: totalTime, // Tempo de download em segundos
-      fileId: fileId // ID do arquivo no Google Drive
+      downloadUrl: folderLink,
+      timer: totalTime,
+      fileId: fileId
     });
   });
 });
